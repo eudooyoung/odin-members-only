c5f98dca1a5cd2fde49193c9bf731856f74903d3
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-05-25 11:21:44 +0900

    feat: add populate query

--- src/db/populate.ts
+++ src/db/populate.ts
@@ -1,10 +1,16 @@
 #! /user/bin/env node
 
 import pg = require("pg");
+import fs = require("node:fs");
 
 const SQL = `
-create table if not exists user (
-    user_id int primary key generated always as identity,
+drop table if exists person
+                   , message;
+
+set timezone = 'Asia/Seoul';
+
+create table if not exists person (
+    person_id int primary key generated always as identity,
     first_name varchar (50) not null,
     last_name varchar (50) not null,
     username varchar (255) not null unique,
@@ -14,15 +20,54 @@ create table if not exists user (
     created_at timestamptz default now()
 );
 
-create table if not eixsts message (
+create table if not exists message (
     message_id int primary key generated always as identity,
-    user_id int not null,
+    person_id int not null,
     title varchar (100) not null,
     content text not null,
     created_at timestamptz default now(),
-    constraint fk_user
-        foreign key(user_id)
-        references user(user_id)
+    constraint fk_person
+        foreign key(person_id)
+        references person(person_id)
         on delete cascade
 );
 `;
+
+const main = async () => {
+  console.log("seeding start...");
+  let client;
+  if (process.argv[2] === "local") {
+    console.log("preparing client for local db...");
+    client = new pg.Client({
+      connectionString: process.env.LOCAL_DB_URL,
+    });
+  } else {
+    console.log("preparing client for remote db...");
+    client = new pg.Client({
+      user: process.env.REMOTE_DB_USER,
+      password: process.env.REMOTE_DB_PW,
+      host: process.env.REMOTE_EB_HOST,
+      port: Number(process.env.REMOTE_DB_PORT),
+      database: process.env.REMOTE_DB_NAME,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+      },
+    });
+  }
+  console.log("client build successfully");
+  console.log("start trying to connect to db...");
+  try {
+    await client.connect();
+    console.log("connected");
+    await client.query(SQL);
+    console.log("tables have been successfully populated!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+    console.log("end connection");
+  }
+};
+
+void main();
